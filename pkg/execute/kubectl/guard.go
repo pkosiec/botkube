f6d37f1426d5748644647ebc31702bc7c00195b3
Paweł Kosiec <pawel.kosiec@gmail.com>
2022-10-06 13:43:40 +0200

    WIP

--- pkg/execute/kubectl/guard.go
+++ pkg/execute/kubectl/guard.go
@@ -1,22 +1,37 @@
 package kubectl
 
 import (
+	"github.com/sirupsen/logrus"
 	"k8s.io/client-go/discovery"
-	"k8s.io/client-go/kubernetes"
 )
 
 type Resource struct {
+	// Plural
 	Name       string
 	Namespaced bool
+
+	SlashSeparatedInCommand bool
 }
 
 type CommandGuard struct {
-	k8sCli       kubernetes.Interface
+	log          logrus.FieldLogger
 	discoveryCli discovery.DiscoveryInterface
 }
 
-func (g *CommandGuard) GetAllowedResourcesForVerb(selectedVerb string, allConfiguredResources []string) ([]Resource, error) {
-	resList, err := g.discoveryCli.ServerPreferredResources()
-
-	res
+func NewCommandGuard(log logrus.FieldLogger, discoveryCli discovery.DiscoveryInterface) *CommandGuard {
+	return &CommandGuard{log: log, discoveryCli: discoveryCli}
 }
+
+func (g *CommandGuard) GetAllowedResourcesForVerb(selectedVerb string, allConfiguredResources []string) ([]Resource, error) {
+	//resList, err := g.discoveryCli.ServerPreferredResources()
+
+	return nil, nil
+}
+
+func (g *CommandGuard) GetResourceDetails(selectedVerb, resourceType string) (Resource, error) {
+	return Resource{}, nil
+}
+
+//func (g *CommandGuard) GetCommandsForResource(resourceType string, allConfiguredVerbs []string) ([], error) {
+//	return nil, nil
+//}
